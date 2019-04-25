f187c94bbdfff1c7d24aa678a9c8269d5e4071be
Jared Williams <jdwux80@gmail.com>
2019-04-25 13:48:51 -0400

    Initial HTML added and Firebase initilaizer added to JS doc.

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD-KpSGyr4-NWrCLFUOMU_1osYBJ1McHTs",
+    authDomain: "time-railz-train-scheduler.firebaseapp.com",
+    databaseURL: "https://time-railz-train-scheduler.firebaseio.com",
+    projectId: "time-railz-train-scheduler",
+    storageBucket: "time-railz-train-scheduler.appspot.com",
+    messagingSenderId: "1098841836940"
+  };
+  firebase.initializeApp(config);
