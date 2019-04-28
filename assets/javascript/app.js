759a8c7db776a0f826b954ef8bedaa68af7099be
Jared Williams <jdwux80@gmail.com>
2019-04-27 21:30:10 -0400

    Updated script after resolving undeclared variables issue. Renamed the object properties and resolved the append row issue by adding each form field variable.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,45 +17,47 @@ $("#add-train").on("click", function (event) {
     // prevent form submit
     event.preventDefault();
     // Grab form inputs
-    var trainName = $("#train-name-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var firstTrainTime = $("#first-time-input").val().trim();
-    var frequency = $("#frequency-input").val().trim();
+    var trainNameText = $("#train-name-input").val().trim();
+    var destinationText = $("#destination-input").val().trim();
+    var firstTrainTimeText = $("#first-time-input").val().trim();
+    var frequencyText = $("#frequency-input").val().trim();
     // C-Logged ^^^^
 
     // Make inputs a temp object for train data
-    var newTrain = {
-        newTrainName: trainName,
-        city: destination,
-        firstArrival: firstTrainTime,
-        freq: frequency
+    var newTrainToAdd = {
+        newTrainName: trainNameText,
+        city: destinationText,
+        firstArrival: firstTrainTimeText,
+        freq: frequencyText
     };
 
     // Add new train data to Firebase DB
-    database.ref().push(newTrain);
+    database.ref().push(newTrainToAdd);
     // *** Confirmed ^
     alert("Your train was successfully added to the schedule.");
     //Clear form fields
     $("#train-name-input").val("");
     $("#destination-input").val("");
-    $("#first-time-input").val("");
     $("#frequency-input").val("");
+    $("#first-time-input").val("");
 });
-
 
 // EVENT HANDLER
 database.ref().on("child_added", function (childSnapshot) {
-      // Store everything into a variable.
-  trainName = childSnapshot.val().newTrainName;
-  destination = childSnapshot.val().city;
-  firstTrainTime = childSnapshot.val().firstArrival;
-  frequency = childSnapshot.val().freq;
+    // Store form field inputs
+    var trainNameText = childSnapshot.val().newTrainName;
+    var destinationText = childSnapshot.val().city;
+    var firstTrainTimeText = childSnapshot.val().firstArrival;
+    var frequencyText = childSnapshot.val().freq;
 
     // Create a new row
-    newRow = $("<tr>").append(
-        $("<td>").text(newTrain),
-  );
+    var newTrainToAdd = $("<tr>").append(
+        $("<td>").text(trainNameText),
+        $("<td>").text(destinationText),
+        $("<td>").text(frequencyText),
+        $("<td>").text(firstTrainTimeText),
+    );
 
-  // Append new row to table
-  $("#train-table > tbody").append(newTrain)
+    // Append row to table
+    $("#train-table > tbody").append(newTrainToAdd)
 });
