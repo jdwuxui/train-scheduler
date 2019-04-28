6747afc652653e6b02233e6d9f434c843ae58d7c
Jared Williams <jdwux80@gmail.com>
2019-04-27 20:00:03 -0400

    Modified the HTML so rows can be appended. JS script has successfully added the data to firebase with the click event handler and functions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,96 +1,54 @@
 // Initialize Firebase
-// var config = {
-//     apiKey: "AIzaSyD-KpSGyr4-NWrCLFUOMU_1osYBJ1McHTs",
-//     authDomain: "time-railz-train-scheduler.firebaseapp.com",
-//     databaseURL: "https://time-railz-train-scheduler.firebaseio.com",
-//     projectId: "time-railz-train-scheduler",
-//     storageBucket: "time-railz-train-scheduler.appspot.com",
-//     messagingSenderId: "1098841836940"
-// };
-// firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyD-KpSGyr4-NWrCLFUOMU_1osYBJ1McHTs",
+    authDomain: "time-railz-train-scheduler.firebaseapp.com",
+    databaseURL: "https://time-railz-train-scheduler.firebaseio.com",
+    projectId: "time-railz-train-scheduler",
+    storageBucket: "time-railz-train-scheduler.appspot.com",
+    messagingSenderId: "1098841836940"
+};
+firebase.initializeApp(config);
 
+// Firebase global var
+var database = firebase.database();
 
 // Capture submit button click
 $("#add-train").on("click", function (event) {
     // prevent form submit
     event.preventDefault();
-    // Capture User Inputs and store them into variables
+    // Grab form inputs
     var trainName = $("#train-name-input").val().trim();
     var destination = $("#destination-input").val().trim();
     var firstTrainTime = $("#first-time-input").val().trim();
     var frequency = $("#frequency-input").val().trim();
+    // C-Logged ^^^^
 
-    // Confirm vars received
-    // console.log(trainName);
-    // console.log(destination);
-    // console.log(firstTrainTime);
-    // console.log(frequency);
+    // Make inputs a temp object for train data
+    var newTrain = {
+        newTrainName: trainName,
+        city: destination,
+        firstArrival: firstTrainTime,
+        freq: frequency
+    };
 
-    // Output data into table
+    // Add new train data to Firebase DB
+    database.ref().push(newTrain);
+    // *** Confirmed ^
+    alert("Your train was successfully added to the schedule.");
+    //Clear form fields
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-time-input").val("");
+    $("#frequency-input").val("");
 });
 
+// EVENT HANDLER
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
     
-
-
-// Assume the following situations.
-
-// (TEST 1)
-// First Train of the Day is 3:00 AM
-// Assume Train comes every 3 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:18 -- 2 minutes away
-
-// (TEST 2)
-// First Train of the Day is 3:00 AM
-// Assume Train comes every 7 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:21 -- 5 minutes away
-
-
-// ==========================================================
-
-// Solved Mathematically
-// Test case 1:
-// 16 - 00 = 16
-// 16 % 3 = 1 (Modulus is the remainder)
-// 3 - 1 = 2 minutes away
-// 2 + 3:16 = 3:18
-
-// Solved Mathematically
-// Test case 2:
-// 16 - 00 = 16
-// 16 % 7 = 2 (Modulus is the remainder)
-// 7 - 2 = 5 minutes away
-// 5 + 3:16 = 3:21
-
-// // Assumptions
-// var tFrequency = 3;
-
-// // Time is 3:30 AM
-// var firstTime = "03:30";
-
-// // First Time (pushed back 1 year to make sure it comes before current time)
-// var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-// console.log(firstTimeConverted);
-
-// // Current Time
-// var currentTime = moment();
-// console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-// // Difference between the times
-// var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-// console.log("DIFFERENCE IN TIME: " + diffTime);
-
-// // Time apart (remainder)
-// var tRemainder = diffTime % tFrequency;
-// console.log(tRemainder);
-
-// // Minute Until Train
-// var tMinutesTillTrain = tFrequency - tRemainder;
-// console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-// // Next Train
-// var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-// console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    // Store as vars
+    // var trainName = childSnapshot.val().newTrainName;
+    // var destination = childSnapshot.val().city;
+    // var firstTrainTime = childSnapshot.val().firstArrival;
+    // var frequency = childSnapshot.val().freq;
+})
